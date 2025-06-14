c8b7715b81c0168409fb15ccb1974fd905e63b5e
Ethan Lin <ethan.yo.lin@gmail.com>
2025-06-14 08:21:56 -0500

    Update headers and tab names

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Ethan Lin Website';
+export const SITE_DESCRIPTION = 'Ethan Lin is a 4th year undergraduate student pursuing a double major in Computer Science and Electrical and Computer Engineering at Cornell. \
+        I am extremely fortunate to work with Professor Jennifer J. Sun. I previously worked with Professor Donald Greenberg on \
+        virtual reality and 3D modeling projects.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
