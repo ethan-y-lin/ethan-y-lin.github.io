51405d3e333e73cc318e50b209a47c79a241693c
Ethan Lin <ethan.yo.lin@gmail.com>
2026-04-16 12:34:22 -0400

    Fix get to GET

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Ethan Lin Website';
-export const SITE_DESCRIPTION = 'Ethan Lin is a 4th year undergraduate student pursuing a double major in Computer Science and Electrical and Computer Engineering at Cornell. \
-        I am extremely fortunate to work with Professor Jennifer J. Sun. I previously worked with Professor Donald Greenberg on \
-        virtual reality and 3D modeling projects.';
+export const SITE_DESCRIPTION = 'Ethan Lin is a 4th year undergraduate student pursuing a major in Computer Science and \
+minor in Electrical and Computer Engineering at Cornell. He works with Jennifer J. Sun on AI for scientific discovery. \
+ He is an incoming CMS PhD Student at Caltech (Fall 2026).';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
