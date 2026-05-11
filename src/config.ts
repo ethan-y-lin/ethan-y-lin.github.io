82cdaeeaea55988ede39eb8df0c5acd13f48c131
Ethan Lin <ethan.yo.lin@gmail.com>
2026-05-11 17:30:26 -0400

    Reset

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Ethan Lin Website';
-export const SITE_DESCRIPTION = 'Ethan Lin is a 4th year undergraduate student pursuing a major in Computer Science and \
-minor in Electrical and Computer Engineering at Cornell. He works with Jennifer J. Sun on AI for scientific discovery. \
- He is an incoming CMS PhD Student at Caltech (Fall 2026).';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
