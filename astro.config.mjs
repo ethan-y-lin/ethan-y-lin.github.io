15944437f94edc79c188adcd467b9b8f842a15a2
Ethan Lin <ethan.yo.lin@gmail.com>
2026-05-12 17:51:32 -0400

    New personal website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://ethan-y-lin.github.io",
-  integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
